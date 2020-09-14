392df86e5f01c5cb21b4d57a7a9f200b42744e9f
kovacsgaborab <kovacsgaborab@gmail.com>
2020-09-14 11:38:23 +0200

    Beolvas

--- B01KovácsGáborÁbel/Program.cs
+++ B01KovácsGáborÁbel/Program.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
 namespace B01KovácsGáborÁbel
 {
     class Program
     {
+        static int[] adat = new int[1000];
         static void Main(string[] args)
         {
+            Beolvas();
+            Minimumertek();
+            Egyediek();
+
+            Console.ReadKey();
+        }
+
+
+
+        private static void Beolvas()
+        {
+            StreamReader sr = new StreamReader("adatok.dat");
+
+            string[] atmeneti= sr.ReadLine().Split(' '); ;
+
+            while (!sr.EndOfStream)
+            {
+                for (int i = 0; i < adat.Length; i++)
+                {
+                    adat[0] = int.Parse(atmeneti[0]) * 3;
+                }
+            }
+            sr.Close();
         }
     }
 }
